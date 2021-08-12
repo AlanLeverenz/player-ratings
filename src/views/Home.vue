96eea24c3ec0c984267ac925bc9d36dae79b451a
Alan Leverenz <awleverenz@aol.com>
2021-08-12 14:57:10 -0400

    inserted wssl logo

---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/wssl-logo.png">
+    <HelloWorld msg="Welcome to WSSL Evaluations"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
